b54b4ee2a65e3f20bbf649be43e332a5dcfee8d3
Jason Evans <505818+jasonmevans@users.noreply.github.com>
2018-05-19 22:39:40 -0700

    Enlarge test game

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const Minesweeper = require("./minesweeper");
 
-// [0, 1, 0, 0],
-// [0, 1, 1, 0],
-// [0, 0, 0, 0],
-// [0, 0, 0, 1]
-const bombs = [[0, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 1]];
+const bombs = [
+  [0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 0, 0],
+  [0, 1, 0, 0, 0, 1, 0],
+  [0, 0, 0, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0]
+];
 
 const game = new Minesweeper(bombs);
 
-const log = state => console.log(state);
+const log = state => {
+  console.clear();
+  console.log(state.map(line => line.join(" ")).join("\n"));
+};
 
 game.render().then(log);
 game.move(0, 3).then(log);
 game.move(3, 0).then(log);
 game.move(0, 0).then(log);
-game.move(3, 3).then(log);
+game.move(5, 5).then(log);
+game.move(2, 0).then(log);
+game.move(2, 2).then(log);
+game.move(2, 3).then(log);
+game.move(2, 4).then(log);
+game.move(2, 6).then(log);
+game.move(0, 6).then(log);
